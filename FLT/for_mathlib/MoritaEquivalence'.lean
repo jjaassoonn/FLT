1136bcf6ec6b2691bcad526c302b7d06552590e1
zjj <j.jj@j.jj>
2024-06-17 19:36:56 +0100

    give up

--- FLT/for_mathlib/MoritaEquivalence'.lean
+++ FLT/for_mathlib/MoritaEquivalence'.lean
@@ -168,8 +168,9 @@ mul_one := fun x => by
 
 example : true := rfl
 
-instance : Algebra K (e.functor.obj $ ModuleCat.of R R) where
-  smul := fun k x => ((aFunc R S e).symm $ algebraMap K S k) * x
+noncomputable instance : Algebra K (e.functor.obj $ ModuleCat.of R R) where
+  smul := fun k x =>
+  ((aFunc R S e).symm $ algebraMap K S k) * x
   toFun := fun k => (aFunc R S e).symm $ algebraMap K S k
   map_one' := by
     show (aFunc R S e).symm (algebraMap K S 1) = (aFunc R S e).symm 1
@@ -190,6 +191,12 @@ instance : Algebra K (e.functor.obj $ ModuleCat.of R R) where
     simp only [LinearEquiv.apply_symm_apply, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
     change _ = (aFunc R S e).symm _
     simp only [LinearEquiv.apply_symm_apply]
+
+lemma algebraMap_eR_def (k : K) : algebraMap K (e.functor.obj $ ModuleCat.of R R) k =
+  (aFunc R S e).symm (algebraMap K S k) := rfl
+
+lemma mul_eR_def (x y : e.functor.obj $ ModuleCat.of R R) :
+    x * y = (aFunc R S e).symm ((aFunc R S e x) * (aFunc R S e y)) := rfl
 
 noncomputable def eRIsoS : e.functor.obj (ModuleCat.of R R) ≃ₐ[K] S :=
   { toFun := aFunc R S e
@@ -214,130 +221,8 @@ lemma eRIsoS_comp :
   simp only [AlgEquiv.toRingEquiv_eq_coe, RingEquiv.toRingHom_eq_coe,
     AlgEquiv.toRingEquiv_toRingHom, RingHom.coe_comp, RingHom.coe_coe, AlgEquiv.coe_mk,
     Function.comp_apply, eRIsoS]
-  change _ = f (f.symm _)
-  convert LinearEquiv.apply_symm_apply f (algebraMap K S x) |>.symm
--- set_option maxHeartbeats 1000000 in
--- def division_ring_exists_unique_isSimpleModule
---     (N : ModuleCat R) [IsSimpleModule R N] :
---     ∃ (ring : Ring (e.functor.obj N))
---       (alg : Algebra K (e.functor.obj N)) (iso1 : e.functor.obj N ≃ₐ[K] S),
---       algebraMap K S = iso1.toRingEquiv.toRingHom.comp (algebraMap K $ e.functor.obj N) := by
---   have : IsSimpleModule S (e.functor.obj N) := IsMoritaEquivalent.division_ring.IsSimpleModule.functor R S e N
---   have inst4 := IsSimpleModule.nontrivial S $ e.functor.obj N
---   have H := Module.Free.of_divisionRing S $ e.functor.obj N
---   rw [Module.free_iff_set] at H
---   obtain ⟨s, ⟨b⟩⟩ := H
---   if hs1 : s = ∅
---   then
---     subst hs1
---     have := b.index_nonempty
---     simp only [nonempty_subtype, Set.mem_empty_iff_false, exists_const] at this
---   else
---     obtain ⟨i, hi⟩ := Set.nonempty_iff_ne_empty.mpr hs1
---     have eq0 := IsSimpleOrder.eq_bot_or_eq_top (Submodule.span S {b ⟨i, hi⟩}) |>.resolve_left (by
---       intro h
---       simp only [Submodule.span_singleton_eq_bot] at h
---       exact b.ne_zero ⟨i, hi⟩ h)
---     have eq : s = {i} := by
---       refine le_antisymm ?_ (by simpa)
---       simp only [Set.le_eq_subset, Set.subset_singleton_iff]
---       intro j hj
---       have mem : b ⟨j, hj⟩ ∈ Submodule.span S {b ⟨i, hi⟩} := eq0 ▸ ⟨⟩
---       rw [Submodule.mem_span_singleton] at mem
---       obtain ⟨r, hr⟩ := mem
---       have hr' := congr(b.repr $hr)
---       simp only [LinearMapClass.map_smul, Basis.repr_self, Finsupp.smul_single, smul_eq_mul,
---         mul_one] at hr'
---       by_contra rid
---       have hr' := congr($hr' ⟨i, hi⟩)
---       rw [Finsupp.single_eq_same, Finsupp.single_eq_of_ne (h := by simpa)] at hr'
---       subst hr'
---       simp only [zero_smul] at hr
---       exact b.ne_zero _ hr.symm |>.elim
-
---     subst eq
---     let f : e.functor.obj N ≃ₗ[S] S := b.repr ≪≫ₗ
---     { toFun := fun v => v ⟨i, by simp⟩
---       map_add' := fun x y => by simp
---       map_smul' := fun x y => by simp
---       invFun := fun x => Finsupp.single ⟨i, by simp⟩ x
---       left_inv := fun x => by ext; simp
---       right_inv := fun x => by simp }
-
---     have : FiniteDimensional.finrank S (e.functor.obj N) = 1 := by
---       rw [FiniteDimensional.finrank_eq_card_basis b]
---       simp
-
---     letI : Ring (e.functor.obj N) :=
---       { __ := (inferInstance : AddCommGroup (e.functor.obj N))
---         mul := fun x y => f.symm (f x * f y)
---         left_distrib := fun x y z => by
---           show f.symm _ = f.symm _ + f.symm _
---           simp only [map_add, mul_add]
---         right_distrib := fun x y z => by
---           show f.symm _ = f.symm _ + f.symm _
---           simp only [map_add, add_mul]
---         zero_mul := fun x => by
---           show f.symm _ = _
---           simp only [map_zero, zero_mul]
---         mul_zero := fun x => by
---           show f.symm _ = _
---           simp only [map_zero, mul_zero]
---         mul_assoc := fun x y z => by
---           show f.symm (f (f.symm _) * _) = f.symm (_ * f (f.symm _))
---           simp only [LinearEquiv.apply_symm_apply, EmbeddingLike.apply_eq_iff_eq]
---           rw [mul_assoc]
---         one := f.symm 1
---         one_mul := fun x => by
---           show f.symm (f (f.symm 1) * _) = _
---           simp only [LinearEquiv.apply_symm_apply, one_mul, LinearEquiv.symm_apply_apply]
---         mul_one := fun x => by
---           show f.symm (_ * f (f.symm 1)) = _
---           simp only [LinearEquiv.apply_symm_apply, mul_one, LinearEquiv.symm_apply_apply] }
---     letI : Algebra K (e.functor.obj N) :=
---       { smul := fun k x => (f.symm $ algebraMap K S k) * x
---         toFun := fun k => f.symm $ algebraMap K S k
---         map_one' := by
---           show f.symm (algebraMap K S 1) = f.symm 1
---           simp only [_root_.map_one]
---         map_mul' := fun x y => by
---           simp only [_root_.map_mul]
---           change _ = f.symm _
---           simp only [LinearEquiv.apply_symm_apply]
---         map_zero' := by simp only [map_zero]
---         map_add' := fun x y => by simp only [map_add]
---         commutes' := fun x y => by
---           simp only [RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
---           change f.symm _ = f.symm _
---           simp only [LinearEquiv.apply_symm_apply, EmbeddingLike.apply_eq_iff_eq]
---           exact Algebra.commutes _ _
---         smul_def' := fun k x => by
---           change f.symm _ = _
---           simp only [LinearEquiv.apply_symm_apply, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
---           change _ = f.symm _
---           simp only [LinearEquiv.apply_symm_apply] }
---     letI : Module S (e.functor.obj N) := (e.functor.obj N).isModule
---     let ff : e.functor.obj N ≃ₐ[K] S :=
---       { toFun := f
---         invFun := f.symm
---         left_inv := f.3
---         right_inv := f.4
---         map_mul' := fun x y => by
---           simp only
---           change f (f.symm _) = _
---           simp only [LinearEquiv.apply_symm_apply]
---         map_add' := f.map_add
---         commutes' := fun x => by
---           simp only
---           change (f $ f.symm _) = _
---           simp only [LinearEquiv.apply_symm_apply] }
---     refine ⟨inferInstance, inferInstance, ff, ?_⟩
---     ext x
---     simp only [AlgEquiv.toRingEquiv_eq_coe, RingEquiv.toRingHom_eq_coe,
---       AlgEquiv.toRingEquiv_toRingHom, RingHom.coe_comp, RingHom.coe_coe, AlgEquiv.coe_mk,
---       Function.comp_apply, ff]
---     change _ = f (f.symm _)
---     convert LinearEquiv.apply_symm_apply f (algebraMap K S x) |>.symm
+  change _ = (aFunc R S e) ((aFunc R S e).symm _)
+  convert LinearEquiv.apply_symm_apply (aFunc R S e) (algebraMap K S x) |>.symm
 
 instance : e.functor.Additive :=
   Functor.additive_of_preserves_binary_products _
@@ -366,57 +251,11 @@ lemma isSimpleModule_iff_injective_or_eq_zero (M : ModuleCat R) :
 
 open ZeroObject
 
--- variable {R S} in
--- instance _root_.CategoryTheory.Equivalence.nontrivial
---     (M : ModuleCat R) [h : Nontrivial M] : Nontrivial (e.functor.obj M) := by
---   obtain ⟨m, n, h⟩ := h
---   by_contra inst1
---   rw [not_nontrivial_iff_subsingleton] at inst1
---   let iso1 : e.functor.obj M ≅ 0 :=
---   { hom := ⟨⟨fun _ => 0, by intros; simp⟩, by intros; simp⟩
---     inv := ⟨⟨fun _ => 0, by intros; simp⟩, by intros; simp⟩
---     hom_inv_id := by intros; ext; exact Subsingleton.elim _ _
---     inv_hom_id := by intros; ext; simp only [ModuleCat.coe_comp, Function.comp_apply,
---       Limits.id_zero]; rfl }
---   let iso2 : M ≅ 0 := calc M
---       _ ≅ e.inverse.obj (e.functor.obj M) := e.unitIso.app M
---       _ ≅ e.inverse.obj 0 := e.inverse.mapIso iso1
---       _ ≅ 0 := e.inverse.mapZeroObject
---   let iso3 : (0 : ModuleCat R) ≅ ModuleCat.of R PUnit :=
---   { hom := ⟨⟨fun _ => 0, by intros; simp⟩, by intros; simp⟩
---     inv := ⟨⟨fun _ => 0, by intros; simp⟩, by intros; simp⟩
---     hom_inv_id := by intros; ext; simp only [ModuleCat.coe_comp, Function.comp_apply,
---       Limits.id_zero]; rfl
---     inv_hom_id := by intros; ext; exact Subsingleton.elim _ _ }
---   refine h $ LinearEquiv.injective iso2.toLinearEquiv $
---     LinearEquiv.injective iso3.toLinearEquiv $ Subsingleton.elim _ _
-
-
--- lemma IsSimpleModule.functor
---     (M : ModuleCat R) [simple_module : IsSimpleModule R M] : IsSimpleModule S (e.functor.obj M) := by
---   rw [isSimpleModule_iff_injective_or_eq_zero] at simple_module ⊢
---   rcases simple_module with ⟨nontriv, H⟩
---   refine ⟨e.nontrivial M, fun N f => ?_⟩
---   let F := e.unit.app M ≫ e.inverse.map f
---   rcases H _ F with H|H
---   · simp only [Functor.id_obj, Functor.comp_obj, Preadditive.IsIso.comp_left_eq_zero, F] at H
---     replace H : e.inverse.map f = e.inverse.map 0 := by simpa using H
---     exact Or.inl $ e.inverse.map_injective H
---   · simp only [Functor.id_obj, Functor.comp_obj, F] at H
---     refine Or.inr ?_
---     rw [← ModuleCat.mono_iff_injective] at H ⊢
---     have m1 : Mono (e.functor.map $ e.unit.app M ≫ e.inverse.map f) := e.functor.map_mono _
---     simp only [Functor.id_obj, Functor.comp_obj, Functor.map_comp, Equivalence.fun_inv_map,
---       Equivalence.functor_unit_comp_assoc] at m1
---     exact mono_of_mono f (e.counitInv.app N)
-
-instance : SMul K (End $ ModuleCat.of R R) where
-  smul k (f : R →ₗ[R] R) := k • f
+instance : SMul K (End $ ModuleCat.of R R) :=
+  inferInstanceAs $ SMul K (R →ₗ[R] R)
 
 @[simp]
 lemma algebra_smul_end_def (k : K) (f : R →ₗ[R] R) (r : R) : (k • f) r = k • f r := rfl
-
-#synth Algebra K $ R →ₗ[R] R
 
 @[simps]
 def algebraMapEnd : K →+* End (ModuleCat.of R R) where
@@ -436,6 +275,31 @@ def algebraMapEnd : K →+* End (ModuleCat.of R R) where
     simp only [LinearMap.coe_mk, AddHom.coe_mk]
     rw [LinearMap.add_apply]
     simp only [add_smul, LinearMap.coe_mk, AddHom.coe_mk]
+
+variable {R S} in
+def aux1 : End (ModuleCat.of R R) ≃+* End (e.functor.obj $ ModuleCat.of R R) where
+  toFun f := e.functor.map f
+  invFun g := e.unit.app _ ≫ e.inverse.map g ≫ e.unitInv.app _
+  left_inv := by
+    intro f
+    simp only [Functor.comp_obj, Equivalence.inv_fun_map, Functor.id_obj, Category.assoc,
+      Iso.hom_inv_id_app, Category.comp_id]
+    rw [← Category.assoc]
+    change (e.unit ≫ e.unitInv).app _ ≫ _ = _
+    simp
+  right_inv := by
+    intro g
+    simp only [Functor.comp_obj, Functor.map_comp, Equivalence.fun_inv_map, Functor.id_obj,
+      Category.assoc, Equivalence.counitInv_functor_comp, Category.comp_id]
+    exact e.functor_unit_comp_assoc (ModuleCat.of R R) g
+  map_mul' x y := by simp
+  map_add' x y := by simp only; rw [e.functor.map_add]
+
+@[simps!]
+noncomputable def algebraMapEnd' : K →+* End (e.functor.obj $ ModuleCat.of R R) :=
+  RingHom.comp
+    (aux1 R e)
+    (algebraMapEnd K R)
 
 instance : Algebra K (End $ ModuleCat.of R R) where
   toRingHom := algebraMapEnd K R
@@ -458,6 +322,37 @@ instance : Algebra K (End $ ModuleCat.of R R) where
     rw [Algebra.smul_def, ← smul_eq_mul]
     erw [← f.map_smul]
     rw [smul_eq_mul, ← Algebra.smul_def]
+
+noncomputable instance : Algebra K (End $ e.functor.obj $ ModuleCat.of R R) where
+  toRingHom := algebraMapEnd' K R S e
+  commutes' k (f : _ →ₗ[_] _) := by
+    simp only [algebraMapEnd'_apply, End.mul_def]
+    erw [End.mul_def]
+    simp only [aux1, Functor.comp_obj, RingEquiv.coe_mk, Equiv.coe_fn_mk]
+    apply_fun (e.inverse.map) using e.inverse.map_injective
+    simp only [Functor.map_comp, Equivalence.inv_fun_map, Functor.comp_obj, Functor.id_obj,
+      Category.assoc]
+    refine LinearMap.ext fun x => ?_
+    repeat erw [comp_apply]
+    simp only [algebraMapEnd, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk]
+
+    repeat erw [LinearMap.coe_mk, AddHom.coe_mk]
+    simp only [Functor.comp_obj, Functor.id_obj, AddHom.toFun_eq_coe, LinearMap.coe_toAddHom]
+    rw [Algebra.smul_def, Algebra.smul_def, ← smul_eq_mul, ← smul_eq_mul]
+    have := (e.unitInv.app (ModuleCat.of R R)).map_smul (algebraMap K R k) x
+    erw [← this]
+    have := (e.unitInv.app (ModuleCat.of R R)).map_smul (algebraMap K R k) (e.inverse.map f $ x)
+    erw [← this]
+    change e.unit.app _ (e.unitInv.app _ _) =
+      e.inverse.map _ ((e.unitInv ≫ e.unit).app (ModuleCat.of R R) _)
+    rw [Iso.inv_hom_id, NatTrans.id_app, ModuleCat.id_apply]
+    change (e.unitInv.app _ ≫ e.unit.app _) _ = _
+    simp only [Functor.comp_obj, Functor.id_obj, Iso.inv_hom_id_app, LinearMapClass.map_smul,
+      ModuleCat.id_apply]
+  smul_def' k (f : _ →ₗ[_] _) := rfl
+
+lemma algebraMap_end_eR_def :
+  algebraMap K (End $ e.functor.obj $ ModuleCat.of R R) = (algebraMapEnd' K R S e) := rfl
 
 @[simps]
 def mopToEnd : Rᵐᵒᵖ →ₐ[K] End (ModuleCat.of R R) where
@@ -518,26 +413,26 @@ noncomputable def mopEquivEnd : Rᵐᵒᵖ ≃ₐ[K] End (ModuleCat.of R R) :=
 --     rfl
 --   smul_def' := fun _ _ => rfl
 
--- variable {R S} in
--- def aux1 : End (ModuleCat.of R R) ≃ₐ[K] End (e.functor.obj $ ModuleCat.of R R) where
---   toFun f := e.functor.map f
---   invFun g := e.unit.app _ ≫ e.inverse.map g ≫ e.unitInv.app _
---   left_inv := by
---     intro f
---     simp only [Functor.comp_obj, Equivalence.inv_fun_map, Functor.id_obj, Category.assoc,
---       Iso.hom_inv_id_app, Category.comp_id]
---     rw [← Category.assoc]
---     change (e.unit ≫ e.unitInv).app _ ≫ _ = _
---     simp
---   right_inv := by
---     intro g
---     simp only [Functor.comp_obj, Functor.map_comp, Equivalence.fun_inv_map, Functor.id_obj,
---       Category.assoc, Equivalence.counitInv_functor_comp, Category.comp_id]
---     exact e.functor_unit_comp_assoc (ModuleCat.of R R) g
---   map_mul' x y := by simp
---   map_add' x y := by simp only; rw [e.functor.map_add]
---   commutes' k := rfl
-
+variable {R S} in
+def aux1Algebra : End (ModuleCat.of R R) ≃ₐ[K] End (e.functor.obj $ ModuleCat.of R R) where
+  toFun f := e.functor.map f
+  invFun g := e.unit.app _ ≫ e.inverse.map g ≫ e.unitInv.app _
+  left_inv := by
+    intro f
+    simp only [Functor.comp_obj, Equivalence.inv_fun_map, Functor.id_obj, Category.assoc,
+      Iso.hom_inv_id_app, Category.comp_id]
+    rw [← Category.assoc]
+    change (e.unit ≫ e.unitInv).app _ ≫ _ = _
+    simp
+  right_inv := by
+    intro g
+    simp only [Functor.comp_obj, Functor.map_comp, Equivalence.fun_inv_map, Functor.id_obj,
+      Category.assoc, Equivalence.counitInv_functor_comp, Category.comp_id]
+    exact e.functor_unit_comp_assoc (ModuleCat.of R R) g
+  map_mul' x y := by simp
+  map_add' x y := by simp only; rw [e.functor.map_add]
+  commutes' k := by
+    rfl
 
 -- noncomputabl : Ring (e.functor.obj $ ModuleCat.of R R) :=
 --   division_ring_exists_unique_isSimpleModule K R S e (ModuleCat.of R R) |>.choose
@@ -551,36 +446,80 @@ noncomputable def mopEquivEnd : Rᵐᵒᵖ ≃ₐ[K] End (ModuleCat.of R R) :=
 --   exact this.some.toModuleIso
 
 
-noncomputable def toRingMopEquiv : Nonempty $ Rᵐᵒᵖ ≃ₐ[K] Sᵐᵒᵖ := by
-  obtain ⟨ring_inst, alg_inst, iso1, h⟩ :=
-    division_ring_exists_unique_isSimpleModule K R S e (ModuleCat.of R R)
-  refine Nonempty.intro $ mopEquivEnd K R |>.trans $ AlgEquiv.trans ?_ $ mopEquivEnd K S |>.symm
-  let alg' : Algebra K (End $ e.functor.obj $ ModuleCat.of R R) :=
-  { smul := fun k f =>
-      _
-    toFun := _
-    map_one' := _
-    map_mul' := _
-    map_zero' := _
-    map_add' := _
-    commutes' := _
-    smul_def' := _ }
+noncomputable def aux2
+    (he : ∀ (k : K) (r : e.functor.obj $ ModuleCat.of R R), e.functor.map ({
+      toFun := fun (r : R) => k • r
+      map_add' := fun x y => by
+        simp only [smul_add]
+      map_smul' := fun (x y : R) => by
+        simp only [smul_eq_mul, RingHom.id_apply, Algebra.mul_smul_comm] } :
+        ModuleCat.of R R ⟶ ModuleCat.of R R) r = k • r) :
+    End (e.functor.obj $ ModuleCat.of R R) ≃ₐ[K] End (ModuleCat.of S S) where
+  toFun f := (aFunc R S e).symm.toLinearMap ≫ f ≫ (aFunc R S e).toLinearMap
+  invFun f := (by exact (aFunc R S e).toLinearMap) ≫ f ≫ (aFunc R S e).symm.toLinearMap
+  left_inv := by
+    intro f
+    simp only [Category.assoc]
+    refine LinearMap.ext fun x => ?_
+    repeat erw [comp_apply]
+    repeat erw [(aFunc R S e).symm_apply_apply]
+    rfl
+  right_inv := by
+    intro f
+    simp only [Category.assoc]
+    refine LinearMap.ext fun x => ?_
+    repeat erw [comp_apply]
+    repeat erw [(aFunc R S e).apply_symm_apply]
+    rfl
+  map_mul' := fun x y => by
+    simp only [End.mul_def, Category.assoc]
+    congr 2
+    ext a
+    repeat erw [comp_apply]
+    repeat erw [(aFunc R S e).symm_apply_apply]
+  map_add' := fun x y => by
+    simp only
+    rw [Preadditive.add_comp, Preadditive.comp_add]
+  commutes' k := by
+    simp only
+    refine LinearMap.ext fun x => ?_
+    repeat erw [comp_apply]
+    rw [algebraMap_end_eR_def]
+    change _ = (algebraMapEnd K S k).toFun x
+    simp only [algebraMapEnd'_apply, algebraMapEnd, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk,
+      AddHom.toFun_eq_coe, AddHom.coe_mk]
+    simp only [aux1, Functor.comp_obj, RingEquiv.coe_mk, Equiv.coe_fn_mk]
+    erw [he]
+    change aFunc R S e ((aFunc R S e).symm _) = _
+    simp only [LinearEquiv.apply_symm_apply]
+    erw [(aFunc R S e).apply_symm_apply]
+    rw [Algebra.smul_def]
 
-  let e1 : End (ModuleCat.of R R) ≃ₐ[K] End (e.functor.obj $ ModuleCat.of R R) := sorry
+noncomputable def toAlgMopEquiv : Rᵐᵒᵖ ≃ₐ[K] Sᵐᵒᵖ := by
+  refine mopEquivEnd K R |>.trans $ AlgEquiv.trans ?_ $ mopEquivEnd K S |>.symm
 
+  let e1 : End (ModuleCat.of R R) ≃ₐ[K] End (e.functor.obj $ ModuleCat.of R R) := aux1Algebra K R e
+  let e2 : End (e.functor.obj $ ModuleCat.of R R) ≃ₐ[K] End (ModuleCat.of S S) := aux2 K R S e sorry
 
-noncomputable def toRingEquiv : R ≃+* S where
-  toFun r := toRingMopEquiv R S e (.op r) |>.unop
-  invFun s := toRingMopEquiv R S e |>.symm (.op s) |>.unop
+  refine e1.trans e2
+
+noncomputable def toAlgEquiv : R ≃ₐ[K] S where
+  toFun r := toAlgMopEquiv K R S e (.op r) |>.unop
+  invFun s := toAlgMopEquiv K R S e |>.symm (.op s) |>.unop
   left_inv r := by simp
   right_inv s := by simp
   map_mul' a b := by simp
   map_add' a b := by simp
+  commutes' a := by
+    simp only
+    apply_fun MulOpposite.op using MulOpposite.op_injective
+    exact (toAlgMopEquiv K R S e).commutes a
 
 end division_ring
 
-noncomputable def ringEquivOfDivisionRing
-    (R S : Type u) [DivisionRing R] [DivisionRing S] [IsMoritaEquivalent' R S] :
-    R ≃+* S := division_ring.toRingEquiv R S (equiv R S)
+noncomputable def algEquivOfDivisionRing
+    (R S : Type u) [DivisionRing R] [DivisionRing S] [Algebra K R] [Algebra K S]
+    [h : IsMoritaEquivalent' K R S] :
+    R ≃ₐ[K] S := division_ring.toAlgEquiv K R S (h.out.some)
 
 end IsMoritaEquivalent'
