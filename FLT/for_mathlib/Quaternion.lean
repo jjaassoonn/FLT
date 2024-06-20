981dcd8f95876c806e44f50f51ba01da25255f0e
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2024-06-20 21:30:53 +0100

    Update Quaternion.lean

--- FLT/for_mathlib/Quaternion.lean
+++ FLT/for_mathlib/Quaternion.lean
@@ -1,37 +1,35 @@
 import Mathlib.Algebra.Quaternion -- probably get away with less
-import FLT.for_mathlib.Con
-import FLT.for_mathlib.CrazySimple
 import FLT.for_mathlib.IsCentralSimple
-import Mathlib.RingTheory.Flat.Basic
-import Mathlib.LinearAlgebra.TensorProduct.RightExactness
 
-variable (D : Type*) [DivisionRing D] [Algebra ℚ D]
-variable (a b : ℚ)
-instance: Field ℚ := by exact Rat.instField
-instance isoiso : Module ℚ D := by exact Algebra.toModule
-instance isoisoiso (h1: Module.rank ℚ D = 4): Nonempty (D ≃ₐ[ℚ] QuaternionAlgebra ℚ a b):= by sorry
-instance isoisoiso1: Field (QuaternionAlgebra ℚ a b) := by sorry
-instance isoisoisobala: Algebra ℚ (QuaternionAlgebra ℚ a b) := by sorry
-theorem isoisoisoiso: IsCentralSimple ℚ (QuaternionAlgebra ℚ a b) where
+variable (D : Type) [DivisionRing D] [Algebra ℚ D] [h : IsCentralSimple ℚ D] 
+    [FiniteDimensional ℚ D] (hD : FiniteDimensional.finrank ℚ D = 4)
+open Quaternion TensorProduct BigOperators Classical
+--instance isoisoiso (h1: Module.rank ℚ D = 4): Nonempty (D ≃ₐ[ℚ] QuaternionAlgebra ℚ a b):= by sorry
 
-  is_central z hz := by
+variable (α : Type*) (s : Set α)
+
+theorem Quat_is_CSA: IsCentralSimple ℚ (ℍ[ℚ]) where
+  is_central z hz := by 
     rw [@Subalgebra.mem_center_iff] at hz
-    -- apply? at hz
-    have h1 : ∀ (y : QuaternionAlgebra ℚ a b), (y * z).re = (z * y).re := by
-      exact fun y ↦ congrArg Quaternion.re (hz y)
-    
-    
-    have : ∀ (y : QuaternionAlgebra ℚ a b), (y * z).re = (z * y).re := by sorry
-    have : ∀ (y: QuaternionAlgebra ℚ a b), (y * z).im = (z * y).im := by
-      sorry
-    -- have h1:= QuaternionAlgebra.basisOneIJK (R:= ℚ) a b
-    sorry
+    let eq2 := fun y ↦ congrArg Quaternion.imI (hz y)
+    let eq3 := fun y ↦ congrArg Quaternion.imJ (hz y)
+    let eq4 := fun y ↦ congrArg Quaternion.imK (hz y)
+    simp only [mul_re, mul_imI, mul_imJ, mul_imK] at eq2 eq3 eq4
+    simp [mul_comm] at eq2 eq3 eq4 
+    specialize eq2 ⟨0,0,1,0⟩ 
+    simp only [zero_mul, mul_zero, add_zero, one_mul, zero_add, sub_zero, zero_sub,
+      eq_neg_self_iff] at eq2
+    specialize eq3 ⟨0,0,0,1⟩ 
+    simp only [zero_mul, sub_self, mul_zero, add_zero, mul_one, zero_add, zero_sub,
+      eq_neg_self_iff] at eq3
+    specialize eq4 ⟨0,1,0,0⟩ 
+    simp only [zero_mul, one_mul, zero_add, mul_zero, sub_zero, add_zero, zero_sub,
+      eq_neg_self_iff] at eq4
+    refine ⟨_, id (ext z (↑z.re) rfl eq3 eq4 eq2).symm⟩ 
 
-  is_simple := by sorry
-
-noncomputable instance isoisoisoisoiso: Ring (TensorProduct ℚ D ℂ) := by exact Algebra.TensorProduct.instRing
-noncomputable instance isoisoisoisoiso1: Algebra ℚ (TensorProduct ℚ D ℂ) := by exact
-  Algebra.TensorProduct.instAlgebra
+theorem tensor_C_is_CSA : IsCentralSimple ℂ (ℂ ⊗[ℚ] D) := IsCentralSimple.baseChange ℚ D ℂ 
+ 
 theorem isoisoisoisoisoiso:
-    Nonempty ((TensorProduct ℚ D ℂ)  ≃ₐ[ℚ] (QuaternionAlgebra ℂ a b)) := by
+    Nonempty (ℂ ⊗[ℚ] D  ≃ₐ[ℂ] ℍ[ℂ]) := by
+  
   sorry
