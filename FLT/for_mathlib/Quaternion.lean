f3053f019180e00bc859dcf6f8face9e41929c8c
benjzyq <123026302+benjzyq@users.noreply.github.com>
2024-06-20 18:45:25 +0100

    Update Quaternion.lean

--- FLT/for_mathlib/Quaternion.lean
+++ FLT/for_mathlib/Quaternion.lean
@@ -12,7 +12,22 @@ instance isoiso : Module ℚ D := by exact Algebra.toModule
 instance isoisoiso (h1: Module.rank ℚ D = 4): Nonempty (D ≃ₐ[ℚ] QuaternionAlgebra ℚ a b):= by sorry
 instance isoisoiso1: Field (QuaternionAlgebra ℚ a b) := by sorry
 instance isoisoisobala: Algebra ℚ (QuaternionAlgebra ℚ a b) := by sorry
-theorem isoisoisoiso: IsCentralSimple ℚ (QuaternionAlgebra ℚ a b) := by sorry
+theorem isoisoisoiso: IsCentralSimple ℚ (QuaternionAlgebra ℚ a b) where
+
+  is_central z hz := by
+    rw [@Subalgebra.mem_center_iff] at hz
+    -- apply? at hz
+    have h1 : ∀ (y : QuaternionAlgebra ℚ a b), (y * z).re = (z * y).re := by
+      exact fun y ↦ congrArg Quaternion.re (hz y)
+    
+    
+    have : ∀ (y : QuaternionAlgebra ℚ a b), (y * z).re = (z * y).re := by sorry
+    have : ∀ (y: QuaternionAlgebra ℚ a b), (y * z).im = (z * y).im := by
+      sorry
+    -- have h1:= QuaternionAlgebra.basisOneIJK (R:= ℚ) a b
+    sorry
+
+  is_simple := by sorry
 
 noncomputable instance isoisoisoisoiso: Ring (TensorProduct ℚ D ℂ) := by exact Algebra.TensorProduct.instRing
 noncomputable instance isoisoisoisoiso1: Algebra ℚ (TensorProduct ℚ D ℂ) := by exact
