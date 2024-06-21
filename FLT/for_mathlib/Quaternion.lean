723bba9b53925a4007aa7f42c1b445290b5d01fe
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2024-06-21 13:22:01 +0100

    too much rubbish

--- FLT/for_mathlib/Quaternion.lean
+++ FLT/for_mathlib/Quaternion.lean
@@ -11,20 +11,11 @@ variable (α : Type*) (s : Set α)
 theorem Quat_is_CSA: IsCentralSimple ℚ (ℍ[ℚ]) where
   is_central z hz := by 
     rw [@Subalgebra.mem_center_iff] at hz
-    let eq2 := fun y ↦ congrArg Quaternion.imI (hz y)
-    let eq3 := fun y ↦ congrArg Quaternion.imJ (hz y)
-    let eq4 := fun y ↦ congrArg Quaternion.imK (hz y)
-    simp only [mul_re, mul_imI, mul_imJ, mul_imK] at eq2 eq3 eq4
-    simp [mul_comm] at eq2 eq3 eq4 
-    specialize eq2 ⟨0,0,1,0⟩ 
-    simp only [zero_mul, mul_zero, add_zero, one_mul, zero_add, sub_zero, zero_sub,
-      eq_neg_self_iff] at eq2
-    specialize eq3 ⟨0,0,0,1⟩ 
-    simp only [zero_mul, sub_self, mul_zero, add_zero, mul_one, zero_add, zero_sub,
-      eq_neg_self_iff] at eq3
-    specialize eq4 ⟨0,1,0,0⟩ 
-    simp only [zero_mul, one_mul, zero_add, mul_zero, sub_zero, add_zero, zero_sub,
-      eq_neg_self_iff] at eq4
+    let eq2 := congrArg Quaternion.imI (hz ⟨0,0,1,0⟩)
+    let eq3 := congrArg Quaternion.imJ (hz ⟨0,0,0,1⟩)
+    let eq4 := congrArg Quaternion.imK (hz ⟨0,1,0,0⟩)
+    simp only [mul_imI, zero_mul, add_zero, one_mul, zero_add, sub_zero, mul_zero, mul_one,
+      zero_sub, eq_neg_self_iff, mul_imJ, sub_self, mul_imK] at eq2 eq3 eq4
     refine ⟨_, id (ext z (↑z.re) rfl eq3 eq4 eq2).symm⟩ 
 
 theorem tensor_C_is_CSA : IsCentralSimple ℂ (ℂ ⊗[ℚ] D) := IsCentralSimple.baseChange ℚ D ℂ 
