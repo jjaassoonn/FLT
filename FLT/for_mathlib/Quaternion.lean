e02b650dc5441b10b2c50fbcda8a3d07af857a1b
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2024-07-04 12:21:51 +0100

    Update Quaternion.lean

--- FLT/for_mathlib/Quaternion.lean
+++ FLT/for_mathlib/Quaternion.lean
@@ -1,26 +1,43 @@
-import Mathlib.Algebra.Quaternion -- probably get away with less
+import Mathlib.Algebra.QuaternionBasis
 import FLT.for_mathlib.IsCentralSimple
 
-variable (D : Type) [DivisionRing D] [Algebra ℚ D] [h : IsCentralSimple ℚ D] 
+variable (D : Type) [Ring D] [Algebra ℚ D] [h : IsCentralSimple ℚ D]
     [FiniteDimensional ℚ D] (hD : FiniteDimensional.finrank ℚ D = 4)
 open Quaternion TensorProduct BigOperators Classical
 --instance isoisoiso (h1: Module.rank ℚ D = 4): Nonempty (D ≃ₐ[ℚ] QuaternionAlgebra ℚ a b):= by sorry
 
-variable (α : Type*) (s : Set α)
+variable (a b : ℚ)
 
 theorem Quat_is_CSA: IsCentralSimple ℚ (ℍ[ℚ]) where
-  is_central z hz := by 
+  is_central z hz := by
     rw [@Subalgebra.mem_center_iff] at hz
     let eq2 := congrArg Quaternion.imI (hz ⟨0,0,1,0⟩)
     let eq3 := congrArg Quaternion.imJ (hz ⟨0,0,0,1⟩)
     let eq4 := congrArg Quaternion.imK (hz ⟨0,1,0,0⟩)
     simp only [mul_imI, zero_mul, add_zero, one_mul, zero_add, sub_zero, mul_zero, mul_one,
       zero_sub, eq_neg_self_iff, mul_imJ, sub_self, mul_imK] at eq2 eq3 eq4
-    refine ⟨_, id (ext z (↑z.re) rfl eq3 eq4 eq2).symm⟩ 
+    refine ⟨_, id (ext z (↑z.re) rfl eq3 eq4 eq2).symm⟩
 
-theorem tensor_C_is_CSA : IsCentralSimple ℂ (ℂ ⊗[ℚ] D) := IsCentralSimple.baseChange ℚ D ℂ 
- 
+
+
+theorem tensor_C_is_CSA : IsCentralSimple ℂ (ℂ ⊗[ℚ] D) := IsCentralSimple.baseChange ℚ D ℂ
+
+instance : FiniteDimensional ℂ (ℂ ⊗[ℚ] D) := Module.Finite.base_change ℚ ℂ D
+
+lemma finrank_4 : FiniteDimensional.finrank ℂ (ℂ ⊗[ℚ] D) = 4 := by
+  have := FiniteDimensional.finrank_tensorProduct (S := ℚ) (M := ℂ) (M' := D)
+  sorry
+
+theorem Gen_Quat_is_CSA: IsCentralSimple ℚ (ℍ[ℚ, a, b]) where
+  is_central := _
+  is_simple := _
+
 theorem isoisoisoisoisoiso:
     Nonempty (ℂ ⊗[ℚ] D  ≃ₐ[ℂ] ℍ[ℂ]) := by
-  
+
   sorry
+
+variable (K E : Type) [Field K] [Ring E] [Algebra K E] [h : IsCentralSimple K E]
+    [FiniteDimensional K E] (hD : FiniteDimensional.finrank K E = 4)
+  
+theorem CSA_is_quat : ∃(a b : K), Nonempty (E ≃ₐ[K] ℍ[K, a, b]) := sorry
