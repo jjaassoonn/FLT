a8312d5f7fbae4c832ce3d32fcaf01421033931f
benjzyq <123026302+benjzyq@users.noreply.github.com>
2024-07-04 15:20:06 +0100

    Update Quaternion.lean

--- FLT/for_mathlib/Quaternion.lean
+++ FLT/for_mathlib/Quaternion.lean
@@ -28,8 +28,17 @@ lemma finrank_4 : FiniteDimensional.finrank ℂ (ℂ ⊗[ℚ] D) = 4 := by
   have := FiniteDimensional.finrank_tensorProduct (S := ℚ) (M := ℂ) (M' := D)
   sorry
 
-theorem Gen_Quat_is_CSA: IsCentralSimple ℚ (ℍ[ℚ, a, b]) where
-  is_central := _
+theorem Gen_Quat_is_CSA(h1:a≠ 0)(h2:b≠ 0): IsCentralSimple ℚ (ℍ[ℚ, a, b]) where
+  is_central z hz:= by
+    rw [@Subalgebra.mem_center_iff] at hz
+    let eq2 := congrArg Quaternion.imI (hz ⟨0,0,1,0⟩)
+    let eq3 := congrArg Quaternion.imJ (hz ⟨0,0,0,1⟩)
+    let eq4 := congrArg Quaternion.imK (hz ⟨0,1,0,0⟩)
+    simp only [QuaternionAlgebra.mul_imI, zero_mul, add_zero, mul_one, zero_sub, mul_zero, sub_self,
+      zero_add, neg_eq_self_iff, mul_eq_zero, QuaternionAlgebra.mul_imJ, sub_zero,
+      QuaternionAlgebra.mul_imK, one_mul, eq_neg_self_iff] at eq2 eq3 eq4
+    simp_all only [ne_eq, false_or]
+    refine ⟨_, id (QuaternionAlgebra.ext z (↑z.re) rfl eq3 eq4 eq2).symm⟩
   is_simple := _
 
 theorem isoisoisoisoisoiso:
